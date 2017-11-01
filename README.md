364f24b5b3258b7d740f1af3b6554e5a09ead18b
natha9404 <natha9404@gmail.com>
2017-11-01 16:16:10 -0500

    inicio repositorio

---
+++ README.md
@@ -0,0 +1,4 @@
+API-test
+========
+
+A Symfony project created on October 21, 2017, 9:12 am.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+
+class DefaultController extends FOSRestController
+{
+    /**
+     * @Rest\Get("/")
+     */
+    public function indexAction(Request $request)
+    {
+        $respuesta = array(
+            'mensaje' => 'Ruta no permitida, por favor consulte la documentación.'
+        );
+        return new View($respuesta, Response::HTTP_BAD_REQUEST);
+    }
+    
+    
+}
